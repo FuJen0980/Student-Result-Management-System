0e651dd20d5ac0248bf81cc296f2a434ac0c129e
Yanjun Qian <mandyqian58@gmail.com>
2024-01-01 14:28:31 -0800

    teacher_homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Body from './pages/homepage';
 import UserListPage from './pages/UserListPage';
 import 'bootstrap/dist/css/bootstrap.css';
 import Teacher_home from './pages/Teacher_homepage';
+import Teacher_input from './pages/teacher_input';
+import Teacher_curve from './pages/Teacher_curve';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/users" element={<UserListPage />} />
           <Route path="/" element={<Body />} />
           <Route path="/teacher_home" element={<Teacher_home />} />
+          <Route path="/teacher_input" element={<Teacher_input />} />
+          <Route path="/teacher_curve" element={<Teacher_curve/>} />
             
         </Routes>
 
---
+++ frontend/src/pages/teacher_input.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Head from './header.js';
+
+const Teacher_input = () => {
+    const homepagestyle = {
+        backgroundImage: "url(" + "https://smd-cms.nasa.gov/wp-content/uploads/2023/06/North-star_celestial-pole-1-jpg.webp?w=4096&format=png" + ")",
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height: '700px'
+    }
+    
+    return (
+        
+        <>
+            <main style = {homepagestyle}>
+                <Head/>
+
+            </main>
+            
+
+        </>
+    );
+}
+
+export default Teacher_input;
