d5ccb9f41146c094f274eb6f61ef718716ef61e0
FuJen0980 <chiangjeff23@gmail.com>
2024-01-10 11:28:57 -0800

    added .env variables and fixed login and register page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,106 +1,122 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import {Row, Form, Button } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
-const LoginPage = () => {
-    const homepagestyle = {
-        backgroundImage: 'url("https://smd-cms.nasa.gov/wp-content/uploads/2023/06/North-star_celestial-pole-1-jpg.webp?w=4096&format=png")',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        width: '100%',
-        height: '100vh',
-        overflow: 'hidden',
-    }
-    const [UserName, setUserName] = useState(() => {
-        const localusername = localStorage.getItem("Home-UserName");
-        return localusername ? JSON.parse(localusername) : "";
-    });
-
-    const [Password, setPassword] = useState(() => {
-        const localPassword = localStorage.getItem("Home-UserPassword");
-        return localPassword ? JSON.parse(localPassword) : "";
-    });
-    const [login, setlogin] = useState(false);
-    //fasle - teacherhomepage, true - studenthomepage
-    const [InnerHome, setInnerHome] = useState(null);
-
-    useEffect(() => {
-        console.log("Setting Home-UserName:", UserName);
-        localStorage.setItem("Home-UserName", JSON.stringify(UserName));
-        document.getElementById("Home-name").value = JSON.parse(localStorage.getItem("Home-UserName")) || "";
-
-    }, [UserName])
-
-    useEffect(() => {
-        localStorage.setItem("Home-UserPassword", JSON.stringify(Password)); 
-        document.getElementById("Home-password").value = JSON.parse(localStorage.getItem("Home-UserPassword")) || "";
-    }, [Password])
-
-    if (login && !InnerHome) {
-        return <Navigate to="/teacher/home" />;
-    }
-    if (login && InnerHome) {
-       return <Navigate to="/student/home" />;
-    }
-    
-    return (
-        <>
-        <main style = {homepagestyle}>
-            <Row className = {`pt-5`}>
-                <h1 className = {`text-white  text-center`}>Student Result Management System</h1>
-
-            </Row>
-            
-            <Row className='pt-5 offset-4 col-4'>
-              <div className='border border-primary rounded pt-1 pd-2'>
-                <Form>
-                    <fieldset>
-                        <legend><strong className='text-white'>Login</strong></legend>
-
-                                <p><select className={`form-control`} onChange={(event) => {
-                                    const e = event.target.value === "Student";
-                                    setInnerHome(e);
-                                }}>
-                                <option defaultValue = "default">Identity</option>
-                                <option value= "Administraion">Administraion</option>
-                                <option value = "Student">Student</option>
-                            </select></p>
-
-                                <p><input type="text" placeholder='Name' className={`form-control`}  id = "Home-name" onChange={
-                                    (event) => {
-                                        setUserName(event.target.value);
-                                        localStorage.setItem("Home-UserName", JSON.stringify(event.target.value));
-                                    }
-                                } >
-                                </input></p>
-                                <p><input type="text" placeholder='Password' id = "Home-password" className={`form-control`} onChange={
-                                    (event) => {
-                                        setPassword(event.target.value);
-                                        localStorage.setItem("Home-UserPassword", JSON.stringify(event.target.value));
-
-                                    }
-                        }></input></p>
-
-                        <div className={`d-grid gap-2`}>
-                                    <Button href="#" className={`btn-primary pr-5`} onClick={() => {
-                                        setlogin(true); 
-                                    }}>Login</Button>      
-                        </div><br></br>
-
-                        
-                    </fieldset>
-                    
-                </Form>
-                </div>
-                </Row>
-            
-        </main>
-        
-        </>  
-    );
-    
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Don\'t have an account? '}
+      <Link href="http://localhost:3000/register">
+        Register
+      </Link>{' here.'}
+    </Typography>
+  );
 }
-export default LoginPage;
+
+const API_URL = process.env.REACT_APP_API_URL;
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
+  console.log("API",API_URL);
+  const Navigate = useNavigate();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const requestBody = {
+      name: data.get('name'),
+      password: data.get('password'),
+    };
+
+    try {
+      const response = await axios.post(`${API_URL}/auth/authenticate`, requestBody);
+      console.log('Authentication successful. Token:', response.data.token);
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+
+      const decoded = jwtDecode(token);
+      const userRole = decoded.roles[0];
+      console.log(userRole);
+      switch (userRole) {
+        case 'ADMIN':
+          Navigate('/teacher/home');
+          break;
+        case 'STUDENT':
+          Navigate('/student/home');
+          break;
+        case 'TEACHER':
+          Navigate('/teacher/home');
+          break;
+        default:
+          Navigate('/');
+          break;
+      }
+    } catch (error) {
+      console.log('Authentication failed. Error:', error);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh', justifyContent: 'center', alignItems: 'center', backgroundColor: '#dddddd' }}>
+        <CssBaseline />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: '#2c79cb' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                name="name"
+                autoComplete="name"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                Sign In
+              </Button>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
